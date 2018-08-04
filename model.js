aac9d03ed9ccb4c40209bc4bd85b9c441e76aa29
John Kosmicke <kozjob@yahoo.com>
2018-08-04 07:07:49 -0700

    update

--- model.js
+++ model.js
@@ -1,10 +1,51 @@
 
 
 matcherModel = {
+
+	size: 4,
+	cards: [],
+	cardValues: [ "A", "B", "C", "D", "E", "F", "G", "H" ],
+
+	currentId: 1,
+
+	
 	init: function(size){
+		this.size = size || this.size;
+		var numPairs = Math.pow( this.size, 2) / 2;
+		for( var i = 0; i < numPairs; i++ ) this.addPair();
+		this.shuffle();
 	},
+
+	addPair: function(){
+		var value = this.cardValues[ Math.floor( Math.random() * this.cardValues.length )];
+		this.cards.push( new this.Card( this.getId(), value ));
+		this.cards.push( new this.Card( this.getId(), value ));
+	},
+
+
+	shuffle: function(){
+		var currentIndex = this.cards.length, temp, rand;
+		while( currentIndex > 0 ){
+			rand = Math.floor( Math.random() * currentIndex );
+			currentIndex--;
+
+			temp = this.cards[rand];
+			this.cards[rand] = this.cards[currentIndex];
+			this.cards[currentIndex] = temp;
+		}
+	},
+
+	getId: function(){
+		var myId = this.currentId;
+		this.currentId++;
+		return myId;
+	},
+
+
+	Card: function(id, value){
+		this.id = id;
+		this.value = value;
+	},
+
+
 };
-
-
-
-
---
+++ view.js
@@ -0,0 +1,32 @@
+
+matcherView = {
+
+	model:matcherModel,
+
+	init: function() {
+		this.$grid = $('#matcher-grid');
+		this.addCardsToGrid();
+
+		var width = 100 / this.model.size - 2;
+		$('.card').css({
+			width: width + "%"
+		});
+	},
+	
+	addCardsToGrid: function() {
+		for( var i = 0; i < this.model.cards.length; i++ ){
+			var card = this.model.cards[i];
+			var $cardDiv = $('<div class="card"><div class="name">' + card.value + '</div></div>');
+			$cardDiv.data("card-id", card.id);
+			$cardDiv.attr("id", "card-" + card.id);
+			this.$grid.append( $cardDiv );
+		}
+	},
+	
+	updateGameView: function() {
+	},
+
+	addClickHandlers: function() {
+	},
+
+};
